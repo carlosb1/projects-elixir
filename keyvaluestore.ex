f6f664b8a0e014bb4c94b8dc17db3ce41d7f9644
carlosb1 <mcflurry0@gmail.com>
2021-09-13 23:00:36 +0200

    Key value store

---
+++ keyvaluestore.ex
@@ -0,0 +1,16 @@
+defmodule KeyValueStore do
+  use GenServer
+
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    {:noreply, Map.put(state, key, value)}
+  end
+
+  def handle_cast({:get key}, _, state) do
+    {:reply, Map.get(state, key), state}
+  end
+end
+
