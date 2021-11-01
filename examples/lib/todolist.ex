d39299dd2d07a70d245234fe6c5504410f9077a4
carlosb1 <mcflurry0@gmail.com>
2021-11-01 17:57:42 +0100

    Example todolist with maps

--- examples/lib/todolist.ex
+++ examples/lib/todolist.ex
@@ -23,3 +23,51 @@ defmodule MultiDict do
     Map.get(dict, key, [])
   end
 end
+
+defmodule TodoListv2 do
+  defstruct auto_id: 1, entries: %{}
+  def new(), do: %TodoList{}
+
+  def new(entries \\ []) do
+    Enum.reduce(entries, %TodoListv2{}, fn entry, todo_list_acc ->
+      add_entry(todo_list_acc, entry)
+    end)
+
+    # Enum.reduce(entries, %TodoListv2(), &add_entry(&2, &1))
+  end
+
+  def add_entry(todo_list, entry) do
+    IO.inspect(label: "before")
+    IO.inspect(label: entry)
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+    %TodoListv2{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry)
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoListv2{todo_list | entries: new_entries}
+    end
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+end
+
+defmodule TodoListv2.CsvImporter do
+end
