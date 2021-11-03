b91aa6b855f1491df953bdf399bb4f7e46d4f14f
Carlos Baez <carlosb@albora.io>
2021-11-03 19:42:21 +0100

    Added todoserver eample

---
+++ examples/lib/todoserver.ex
@@ -0,0 +1,37 @@
+defmodule TodoServer do
+  def start do
+    spawn(fn -> loop(TodoListv2.new()) end)
+  end
+
+  defp loop(todo_list) do
+    new_todo_list =
+      receive do
+        message -> process_message(todo_list, message)
+      end
+
+    loop(new_todo_list)
+  end
+
+  defp process_message(todo_list, {:add_entry, new_entry}) do
+    TodoListv2.add_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:entries, caller, date}) do
+    send(caller, {:todo_entries, TodoListv2.entries(todo_list, date)})
+    todo_list
+  end
+
+  def add_entry(todo_server, new_entry) do
+    send(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    send(todo_server, {:entries, self(), date})
+
+    receive do
+      {:todo_entries, entries} -> entries
+    after
+      5000 -> {:error, :timeout}
+    end
+  end
+end
