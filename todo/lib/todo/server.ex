92748681c678a0233c09d8f57400c31a65d3110c
Carlos Baez <carlosb@albora.io>
2021-11-07 20:20:34 +0100

    Added database info

--- todo/lib/todo/server.ex
+++ todo/lib/todo/server.ex
@@ -40,7 +40,7 @@ defmodule Todo.Server do
   use GenServer
 
   def start do
-    GenServer.start(Todo.List, nil)
+    GenServer.start(__MODULE__, nil)
   end
 
   def add_entry(todo_server, new_entry) do
@@ -51,17 +51,16 @@ defmodule Todo.Server do
     GenServer.call(todo_server, {:entries, date})
   end
 
-  @impl GenServer
   def init(_) do
     {:ok, Todo.List.new()}
   end
 
-  @impl GenServer
-  def handle_cast({:add_entry, new_entry}, todo_list) do
-    {:noreply, Todo.List.add_entry(todo_list, new_entry)}
+  def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
+    new_list = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}}
   end
 
-  @impl GenServer
   def handle_call({:entries, date}, todo_list) do
     {:reply, Todo.List.entries(todo_list, date), todo_list}
   end
