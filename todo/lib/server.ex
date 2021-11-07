fbb280135884ecb80dfaf607e2148b9895f02b0b
Carlos Baez <carlosb@albora.io>
2021-11-07 18:25:50 +0100

    server to genserver

--- todo/lib/server.ex
+++ todo/lib/server.ex
@@ -1,4 +1,4 @@
-defmodule Todo.Server do
+defmodule Todo.Server2 do
   def start do
     spawn(fn -> loop(Todo.List.new()) end)
   end
@@ -33,5 +33,36 @@ defmodule Todo.Server do
     after
       5000 -> {:error, :timeout}
     end
+  end
+end
+
+defmodule Todo.Server do
+  use GenServer
+
+  def start do
+    GenServer.start(Todo.List, nil)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+  @impl GenServer
+  def init(_) do
+    {:ok, Todo.List.new()}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    {:noreply, Todo.List.add_entry(todo_list, new_entry)}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, todo_list) do
+    {:reply, Todo.List.entries(todo_list, date), todo_list}
   end
 end
