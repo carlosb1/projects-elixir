3ca0fbf07081df82b8a853479b7741c9e5385817
carlosb1 <mcflurry0@gmail.com>
2021-12-06 18:42:39 +0100

    Fixed cache test

--- todo/test/todo_cache_test.exs
+++ todo/test/todo_cache_test.exs
@@ -2,7 +2,7 @@ defmodule TodoCacheTest do
   use ExUnit.Case
 
   test "to-do operations" do
-    {:ok, :cache} = Todo.Cache.start()
+    {:ok, cache} = Todo.Cache.start()
     alice = Todo.Cache.server_process(cache, "alice")
     Todo.Server.add_entry(alice, %{date: ~D[2018-12-19], title: "Dentist"})
     entries = Todo.Server.entries(alice, ~D[2018-12-19])
