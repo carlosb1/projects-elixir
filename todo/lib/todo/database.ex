258a91c4824379c7616e4334f516cbee32ad737b
carlosb1 <mcflurry0@gmail.com>
2021-12-08 23:56:09 +0100

    Added databaseworker

--- todo/lib/todo/database.ex
+++ todo/lib/todo/database.ex
@@ -47,3 +47,52 @@ defmodule Todo.Database do
     Path.join(@db_folder, to_string(key))
   end
 end
+
+defmodule Todo.DatabaseWorker do
+  use GenServer
+
+  def start(db_folder) do
+    GenServer.start(__MODULE__, nil, name: db_folder)
+  end
+
+  def store(key, data) do
+    GenServer.cast(__MODULE__, {:store, key, data})
+  end
+
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  def init(_) do
+    File.mkdir_p!(@db_folder)
+    {:ok, nil}
+  end
+
+  def handle_cast({:store, key, data}, state) do
+    spawn(fn ->
+      key
+      |> file_name()
+      |> File.write!(:erlang.term_to_binary(data))
+    end)
+
+    {:noreply, state}
+  end
+
+  def handle_call({:get, key}, caller, state) do
+    spawn(fn ->
+      data =
+        case File.read(file_name(key)) do
+          {:ok, contents} -> :erlang.binary_to_term(contents)
+          _ -> nil
+        end
+
+      GenServer.reply(caller, data)
+    end)
+
+    {:noreply, state}
+  end
+
+  defp file_name(key) do
+    Path.join(@db_folder, to_string(key))
+  end
+end
