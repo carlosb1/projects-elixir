7a11d2adb8b807cd9deec273f8fd4ecb2d4edfb7
carlosb1 <mcflurry0@gmail.com>
2021-12-09 22:18:59 +0100

    Refactor database to permit workers

--- todo/lib/todo/database.ex
+++ todo/lib/todo/database.ex
@@ -56,15 +56,20 @@ defmodule Todo.Database2 do
   end
 
   def store(key, data) do
-    GenServer.cast(__MODULE__, {:store, key, data})
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.store(key, data)
   end
 
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.get(key)
   end
 
-  def init(_) do
-    {:ok, nil}
+  def init(db_folder) do
+    File.mkdir_p!(db_folder)
+    {:ok, start_workers()}
   end
 
   def handle_cast({:store, key, data}, state) do
@@ -73,40 +78,50 @@ defmodule Todo.Database2 do
 
   def handle_call({:get, key}, caller, state) do
     {:noreply, state}
+  end
+
+  defp choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key})
+  end
+
+  defp start_workers() do
+    for index <- 1..3, into: %{} do
+      {:ok, pid} = Todo.DatabaseWorker.start(@db_folder)
+      {:index - 1, pid}
+    end
   end
 end
 
 defmodule Todo.DatabaseWorker do
   use GenServer
 
-  def start(db_folder) do
-    GenServer.start(__MODULE__, nil, name: db_folder)
+  def start(worker_pid, db_folder) do
+    GenServer.start(worker_pid, db_folder, name: __MODULE__)
   end
 
-  def store(key, data) do
-    GenServer.cast(__MODULE__, {:store, key, data})
+  def store(worker_pid, key, data) do
+    GenServer.cast(worker_pid, {:store, key, data})
   end
 
-  def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+  def get(worker_pid, key) do
+    GenServer.call(worker_pid, {:get, key})
   end
 
   def init(db_folder) do
-    File.mkdir_p!(db_folder)
-    {:ok, nil}
+    {:ok, db_folder}
   end
 
-  def handle_cast({:store, key, data}, state) do
+  def handle_cast({:store, key, data}, db_folder) do
     spawn(fn ->
       key
       |> file_name()
       |> File.write!(:erlang.term_to_binary(data))
     end)
 
-    {:noreply, state}
+    {:noreply, db_folder}
   end
 
-  def handle_call({:get, key}, caller, state) do
+  def handle_call({:get, key}, caller, db_folder) do
     spawn(fn ->
       data =
         case File.read(file_name(key)) do
@@ -117,7 +132,7 @@ defmodule Todo.DatabaseWorker do
       GenServer.reply(caller, data)
     end)
 
-    {:noreply, state}
+    {:noreply, db_folder}
   end
 
   defp file_name(key) do
