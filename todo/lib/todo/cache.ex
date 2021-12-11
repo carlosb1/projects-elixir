7c603c88b055a711470c2090f40caa2ee28be985
carlosb1 <mcflurry0@gmail.com>
2021-12-12 00:43:00 +0100

    Added system info.

--- todo/lib/todo/cache.ex
+++ todo/lib/todo/cache.ex
@@ -25,3 +25,32 @@ defmodule Todo.Cache do
     GenServer.call(cache_pid, {:server_process, todo_list_name})
   end
 end
+
+defmodule Todo.Cache2 do
+  use GenServer
+
+  def start_link(_) do
+    IO.puts("Starting to-do cache.")
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def server_process(todo_list_name) do
+    GenServer.call(__MODULE__, {:server_process, todo_list_name})
+  end
+
+  def init(_) do
+    Todo.Database.start()
+    {:ok, %{}}
+  end
+
+  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
+    case Map.fetch(todo_servers, todo_list_name) do
+      {:ok, todo_server} ->
+        {:reply, todo_server, todo_servers}
+
+      :error ->
+        {:ok, new_server} = Todo.Server.start()
+        {:reply, new_server, Map.put(todo_servers, todo_list_name, new_server)}
+    end
+  end
+end
